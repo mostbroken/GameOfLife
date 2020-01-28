f61c54730e05078d8d37536b23ff81d5875ac07d
mostbroken <mostbrokenglass@gmail.com>
2020-01-28 22:39:08 +0300

    - optimization

--- sln/GameOfLife/GameOfLife.Wpf/MainWindow.xaml.fs
+++ sln/GameOfLife/GameOfLife.Wpf/MainWindow.xaml.fs
@@ -1,97 +1,119 @@
 ﻿namespace GameOfLife.Wpf
 
+open System
 open System.Windows.Shapes
 open System.Windows.Controls
 open System.Windows.Controls.Primitives
 
+
 type MainWindowXaml = FsXaml.XAML<"MainWindow.xaml">
+
+type CellState =
+    |Alive
+    |Dead
+
+[<Struct>]
+type Position = {
+    X:int
+    Y:int}
 
 [<Struct>]
 type WorldSize ={
     Height:int
     Width:int}
 
-[<Struct>]
-type Cell = 
-    {   IsAlive:bool
-        X:int
-        Y:int }
-
 type Population = 
-    {   Cells: Cell list 
-        Size:WorldSize
+    {   Cells: CellState array
+        WorldSize:WorldSize
         Generation:int64}
 
 type MainWindow() as this =
     inherit MainWindowXaml()
     
     let rnd = System.Random()
-    let frameRatePerSecond = 10
-    let worldSize = { Height = 50; Width = 50 }
+    let frameRatePerSecond = 20
+    let worldSize = { Height = 100; Width = 100 }
+    let populationPercentage = 40
     let getRandomBool percentage = (rnd.Next 100) < percentage
     let mutable started = false
 
-    let generateWorld ws = 
-        let cells = [for r in [0 .. ws.Height-1] do
-                        for c in [0 .. ws.Width-1] do
-                            yield { IsAlive = getRandomBool 30
-                                    X = c
-                                    Y = r}
-                        ]
-        { Cells=cells
-          Size = ws
-          Generation = 1L} 
+    let generatePopulation ws = 
+        let cellsCount = ws.Height * ws.Width
+        let generateCellState () = if getRandomBool populationPercentage then CellState.Alive else CellState.Dead
+        let cells = [|0 .. cellsCount-1|] |> Array.map (fun _ -> generateCellState())
+        { Cells=cells;WorldSize = ws;Generation = 1L} 
                    
-    let getCell world x y = world.Cells|> List.find (fun c-> c.X = x && c.Y = y)
- 
-    let getNeighbors world cell =
-        let up = if cell.Y = 0 then world.Size.Height - 1 else (cell.Y - 1)
-        let down = if cell.Y = world.Size.Height - 1 then 0 else (cell.Y + 1)
-        let left = if cell.X = 0 then world.Size.Width - 1 else (cell.X - 1)
-        let right = if cell.Y = world.Size.Height - 1 then 0 else (cell.Y + 1)
-        let getCellInWorld = getCell world
+    let getCellState population pos = 
+        let shift = pos.Y * population.WorldSize.Width + pos.X
+        population.Cells.[shift]
 
-        [
-            yield getCellInWorld left up 
-            yield getCellInWorld cell.X up
-            yield getCellInWorld right up
-            yield getCellInWorld left cell.Y 
-            yield getCellInWorld right cell.Y
-            yield getCellInWorld left down 
-            yield getCellInWorld cell.X down 
-            yield getCellInWorld right down 
-        ]
+    let getNeighborsStates population cell =
+        let up = if cell.Y = 0 then population.WorldSize.Height - 1 else (cell.Y - 1)
+        let down = if cell.Y = population.WorldSize.Height - 1 then 0 else (cell.Y + 1)
+        let left = if cell.X = 0 then population.WorldSize.Width - 1 else (cell.X - 1)
+        let right = if cell.Y = population.WorldSize.Height - 1 then 0 else (cell.Y + 1)
 
-    let getAliveNeighborsCount world cell =
-        (getNeighbors world cell) |> List.filter (fun c -> c.IsAlive)|> List.length
+        let neighborPositions = 
+            [
+                yield ( left, up  )                
+                yield ( cell.X, up)
+                yield ( right, up)
+                yield ( left, cell.Y )
+                yield ( right, cell.Y)
+                yield ( left, down )
+                yield ( cell.X, down )
+                yield ( right, down )
+            ]|> List.map (fun (x,y) -> {X = x;Y = y})
 
-    let getDeadNeighborsCount world cell =
-        (getNeighbors world cell) |> List.filter (fun c -> c.IsAlive = false)|> List.length
+        neighborPositions |> List.map (fun pos -> getCellState population pos)
 
-    let mutateCell world cell = 
-        let aliveCount = getAliveNeighborsCount world cell
-        let deadCount = getDeadNeighborsCount world cell
+    let mutatePopulation population = 
+        let getPosFromShift shift =
+            let (x,y) = Math.DivRem (shift, population.WorldSize.Width)
+            {X = x;Y = y}
 
-        match (cell.IsAlive, aliveCount, deadCount) with
-        | (false, 3 ,_) -> {cell with IsAlive = true}
-        | (true, l ,_) when l < 2 || l > 3 -> {cell with IsAlive = false}
-        | _ -> cell
+        let mutateCell pos = 
+            let neighborsStates = getNeighborsStates population pos
+            let aliveCount = neighborsStates |> List.filter (fun c -> c = CellState.Alive)|> List.length
+            let deadCount = neighborsStates |> List.filter (fun c -> c = CellState.Dead)|> List.length
+            let cellState = getCellState population pos
 
-    let mutatePopulation world = 
-        let mutatedCells = world.Cells |> List.map (fun c -> mutateCell world c)
-        { world with 
-            Cells = mutatedCells
-            Generation = world.Generation + 1L}
+            match (cellState, aliveCount, deadCount) with
+            | (CellState.Dead, 3 ,_) -> CellState.Alive
+            | (CellState.Alive, ac ,_) when ac < 2 || ac > 3 -> CellState.Dead
+            | _ -> cellState
+
+
+        //population.Cells |> Array.iteri (fun i _ -> (
+        //                                                let pos = getPosFromShift i
+        //                                                let state= mutateCell pos
+        //                                                population.Cells.[i] <- state))
+
+        let mutatedCells = 
+            population.Cells 
+            |> Array.toSeq 
+            |> Seq.mapi (fun i _ -> getPosFromShift i)
+            |> Seq.map (fun pos -> mutateCell pos)
+
+
+        { population with 
+            Cells = mutatedCells |> Seq.toArray
+            Generation = population.Generation + 1L}
         
-    let getCellViews() = [for r in this._worldField.Children do r :?> Rectangle]
+    let getCellViews() = [|for r in this._worldField.Children do r :?> Rectangle|]
 
-    let updateCellView (viewAndCell:(Rectangle*Cell)) =
-        let (r,c) = viewAndCell
-        r.Fill <- if c.IsAlive then System.Windows.Media.Brushes.Green else  System.Windows.Media.Brushes.White
+    let updateCellView (viewAndCellState:(Rectangle*CellState)) =
+        let (v,s) = viewAndCellState
+        let brush = 
+            match s with
+            |CellState.Alive -> System.Windows.Media.Brushes.Green
+            |CellState.Dead -> System.Windows.Media.Brushes.White
+
+        v.Fill <- brush
         ()
 
     let drawWorld world =
-        List.zip (getCellViews()) world.Cells |> List.iter updateCellView
+        Seq.zip (getCellViews()) world.Cells |> Seq.iter updateCellView
 
     let initCellViews worldSize = 
         this._worldField.Rows <- worldSize.Height   
@@ -115,7 +137,7 @@ type MainWindow() as this =
 
 
         let lifeCycle = async {
-                let mutable world = generateWorld worldSize
+                let mutable world = generatePopulation worldSize
             
                 let draw _ = 
                     drawWorld world
